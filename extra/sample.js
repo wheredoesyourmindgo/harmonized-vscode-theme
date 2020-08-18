8c9641d3563b1c9545cf1c67fa9298d793e09cc9
Abe Hendricks <ahendricks@pcwa.net>
2020-08-18 11:53:44 -0700

    sample.js updated

--- extra/sample.js
+++ extra/sample.js
@@ -1,21 +1,10 @@
+import useSWR from 'swr'
 
+function Profile() {
+  const { data, error } = useSWR('/api/user', fetcher)
 
-
-          const btn = document.getElementById('btn')
-          let count = 0
-
-          function render() { 
-            btn.innerText = `Count: ${count}`
-          }
-
-          btn.addEventListener('click', () => {
-            // Count from 1 to 10.
-            if (count < 10) {
-              count += 1
-              render()
-            }
-          })
-
-
-
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return <div className='foobar'>hello {data.name}!</div>
+}
 
